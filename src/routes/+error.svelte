21c7cc2440e5ac6aeae0ddfaec7ac8bea7d1ca64
Ayush Gupta <personal.ayush9@gmail.com>
2024-05-25 17:34:48 +0530

    testing

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+    import '@picocss/pico'
+</script>
+<br>
+<main class="container-fluid">
+    <h1>You knowing entered the wrong path! lel</h1>
+    <a href="/" role="button" class="container-fluid">Go back</a>
+</main>
+
+<style>
+    main {
+        max-width: 95vw;
+        margin: auto;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	let todos = [];
+	let addTodoText;
+
+	function addTodo(){
+		if (addTodoText == undefined || addTodoText.trim() == "") {
+			alert("Todo text can't be empty")
+		} else {	
+			todos[todos.length] = addTodoText;
+			addTodoText = undefined;
+		}
+	}
+
+</script>
+
+<main class="container-fluid">
+	<br>
+	<input bind:value={addTodoText}>
+	<button on:click={ addTodo } class="container-fluid">Add Todo</button>
+	<br>
+	<br>
+{#each todos as todo, index}
+
+	<div class="overflow-auto" role="group">
+		
+		<div role="button">
+			
+			<b>{index+1}. {todo}</b>
+			
+		</div>
+		
+		<button 
+			class="secondary"
+			on:click={ ()=> {todos = [...todos.slice(0,index), ...todos.slice(index+1)];} }>
+			Remove
+		</button>
+		
+		<br>
+	
+	</div>
+
+{/each}
+
+</main>
