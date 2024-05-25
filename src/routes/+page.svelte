64e686fdfa987f549301a908697af8aa96840248
Ayush Gupta <166214458+penguintheguy@users.noreply.github.com>
2024-05-25 22:25:12 +0530

    Update +page.svelte with commets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,57 @@
 <script>
+	// defining an array (can be empty)
 	let todos = ["Add Todo"];
+	//  defining the next input
 	let addTodoText;
 
+	// Function to add values to 'todos' variable
 	function addTodo(){
-		if (addTodoText == undefined || addTodoText.trim() == "") {
+		// if addTodoText is empty or contains onlywhite space
+		if (addTodoText == undefined || addTodoText.trim() == '') {
 			alert("Todo text can't be empty")
 		} else {	
-			todos[todos.length] = addTodoText;
-			addTodoText = undefined;
+			// main logic: if addTodoText is neither empty nor contains onlywhite space
+			// then add addTodoText to the last of todos array
+			// since last element is always at length-1, append to index = length of array
+			todos[todos.length] = addTodoText; 
+			// change addTodoText to undefined as that user can't populate todos with
+			// same data
+			addTodoText = undefined; 
 		}
 	}
-
 </script>
 
+
 <main class="container-fluid">
-	<br>
+<h1 align=center>Copyright © Ayush Gupta { new Date().getFullYear() }</h1>
+	<!-- Bind to addTodoText -->
 	<input bind:value={addTodoText}>
+	<!-- Clicking calls the function -->
 	<button on:click={ addTodo } class="container-fluid">Add Todo</button>
 	<br>
 	<br>
+
+	<!-- loop over the todos array -->
 {#each todos as todo, index}
 
 	<div class="overflow-auto" role="group">
 		
 		<div role="button">
-			
+
+			<!-- Show todo number (position in array + 1) and todo -->
 			<b>{index+1}. {todo}</b>
 			
 		</div>
-		
+
+		<!-- Bind the on click function to call the arrow function 
+		 todos = [...todos.slice(0,index), ...todos.slice(index+1)]; here index
+		 is the index of todo in array todos.
+		 slice the array, include elements from index = 0 to index-1
+		 and from index+1 till last
+		 hence remove the element in todos array
+		 the index of values (data in todos array) after removal of element at particular
+		 index get updated and reflects as well
+		-->
 		<button 
 			class="secondary"
 			on:click={ ()=> {todos = [...todos.slice(0,index), ...todos.slice(index+1)];} }>
